1bab0503d4367b8fb9439c41f599484edc2a8733
mikljohsn <114925102+mikljohsn@users.noreply.github.com>
2023-11-22 11:09:28 +0100

    STA-48 <message>

---
+++ src/main/java/dat3/Course/repository/CourseRepository.java
@@ -0,0 +1,9 @@
+package dat3.Course.repository;
+
+import dat3.Course.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Integer> {
+}
