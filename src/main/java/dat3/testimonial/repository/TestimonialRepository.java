bfc0d61af599e1dd16a9d3aee2f8010c6d0653d3
loui9583 <100029706+loui9583@users.noreply.github.com>
2023-12-04 13:43:02 +0100

    updated course

--- src/main/java/dat3/testimonial/repository/TestimonialRepository.java
+++
@@ -1,9 +0,0 @@
-package dat3.testimonial.repository;
-
-import dat3.testimonial.entity.Testimonial;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TestimonialRepository extends JpaRepository<Testimonial, Integer> {
-}
