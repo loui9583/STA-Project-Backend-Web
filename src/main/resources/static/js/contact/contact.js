fe81fd48f354ba188d5f3b94710e0ddb9ad77eee
simonj1704 <simo915m@stud.kea.dk>
2023-12-07 11:48:43 +0100

    added course type other to form

--- src/main/resources/static/js/contact/contact.js
+++ src/main/resources/static/js/contact/contact.js
@@ -24,6 +24,7 @@ async function sendEmail2() {
     const phoneNumber = document.getElementById("phone-number").value;
     const address = document.getElementById("address").value;
     const course = document.getElementById("course-type").value;
+    const courseOther = document.getElementById("course-type-other").value;
     const licenseCountry = document.getElementById("license-country").value;
     const license = document.getElementById("license").value;
     const ratingExpiryDate = document.getElementById("rating-expiry-date").value;
@@ -34,7 +35,7 @@ async function sendEmail2() {
     const position = document.getElementById("position").value;
     const venue = document.getElementById("venue").value;
     const message = document.getElementById("message").value;
-    const formString = 'Name: ' + fullName + '. Email: ' + email + '. Phone Number: ' + phoneNumber + '. Address: ' + address + '. Course: ' + course + '. License Country: ' + licenseCountry + '. License: ' + license + '. Rating Expiry Date: ' + ratingExpiryDate + '. Flight Hours: ' + flightHours + '. Last Flight Date: ' + lastFlightDate + '. Other MPA: ' + otherMPA + '. Other BPN: ' + otherBPN + '. Position: ' + position + '. Venue: ' + venue + '. Message: ' + message;
+    const formString = 'Name: ' + fullName + '. Email: ' + email + '. Phone Number: ' + phoneNumber + '. Address: ' + address + '. Course: ' + course + '. Course Other:' + courseOther + '. License Country: ' + licenseCountry + '. License: ' + license + '. Rating Expiry Date: ' + ratingExpiryDate + '. Flight Hours: ' + flightHours + '. Last Flight Date: ' + lastFlightDate + '. Other MPA: ' + otherMPA + '. Other BPN: ' + otherBPN + '. Position: ' + position + '. Venue: ' + venue + '. Message: ' + message;
 
     const response = await fetch("api/sendEmail/user-email/" + email + "/email-content/" + formString + "/customer-name/" + fullName, {
         method: 'POST',
